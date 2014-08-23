4657c8c4e38e3f72e1306c1a756d677208b8ffa1
Ryan Thompson <service@aiwebsystems.com>
2014-08-22 23:58:53 -0500

    Small changes to get things started.

---
+++ src/StreamsTheme.php
@@ -0,0 +1,13 @@
+<?php namespace StreamsAddon\Theme\Streams;
+
+use Streams\Core\Addon\ThemeAbstract;
+
+class StreamsTheme extends ThemeAbstract
+{
+    /**
+     * This is an admin theme.
+     *
+     * @var bool
+     */
+    protected $admin = true;
+}
