acec90d249af4dfdb945a21990db45f298cb5b17
Ryan Thompson <service@aiwebsystems.com>
2014-08-31 01:59:20 -0500

    Theme work

--- js/theme/search.js
+++ js/theme/search.js
@@ -5,37 +5,37 @@ Streams.Navigation.lastKeypressTime = 0;
 
 $(function () {
 
-    var toggle = $('#toggle-navigation');
-    var navigation = $('#navigation');
+    var toggle = $('#toggle-search');
+    var search = $('#search');
 
     // When the show / hide is clicked
-    // toggle the navigation panel.
+    // toggle the search panel.
     toggle.bind('click', function (e) {
         e.preventDefault();
 
-        if (toggle.text().toLowerCase() == 'show') {
+        if (toggle.text().toLowerCase() == 'search') {
             toggle.text('ESC or Hide');
-            navigation.toggleClass('closed').toggleClass('open');
+            search.toggleClass('closed').toggleClass('open');
         } else {
-            toggle.text('Show');
-            navigation.toggleClass('closed').toggleClass('open');
+            toggle.text('Search');
+            search.toggleClass('closed').toggleClass('open');
         }
     });
 
-    // If the navigation is open and the user
+    // If the search is open and the user
     // presses ESC, close it.
     $(document).bind('keydown', function (e) {
-        if (navigation.hasClass('open') && e.keyCode == 27) {
+        if (search.hasClass('open') && e.keyCode == 27) {
             toggle.trigger('click');
         }
     });
 
-    // When double SHIFT occurs - open the navigation
+    // When double SHIFT occurs - open the search
     // if it is not already open.
     $(document).on('keyup', function (e) {
         if (new Date() - Streams.Navigation.lastKeypressTime <= Streams.Navigation.doubleTapDelta) {
             if (e.keyCode == 16 && Streams.Navigation.lastKeypress.keyCode == 16) {
-                if (navigation.hasClass('closed')) {
+                if (search.hasClass('closed')) {
                     toggle.trigger('click');
                 }
             }
