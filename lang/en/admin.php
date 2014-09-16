04b902e979546f0abcf8f7f99e5a5edba9cc97fa
Ryan Thompson <service@aiwebsystems.com>
2014-09-16 10:57:31 -0500

    Theme work

---
+++ lang/en/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'copyright'          => '&copy; ' . date('Y') . ' - AI Web Systems, Inc.',
+    'keyboard_shortcuts' => 'Keyboard shortcuts available.',
+    'meta'               => 'Loaded in ' . request_time() . ' using ' . memory_usage(),
+    'results'            => 'Showing results :from - :to of :total',
+];
