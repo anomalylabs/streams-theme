424d3b9cbb24418b97eec6aacdab2be568d22ee0
Ryan Thompson <service@aiwebsystems.com>
2015-01-03 22:55:43 -0600

    Unused

--- src/StreamsTheme.php
+++ src/StreamsTheme.php
@@ -5,19 +5,10 @@ use Anomaly\Streams\Platform\Addon\Theme\Theme;
 class StreamsTheme extends Theme
 {
 
-    protected $slug = 'streams';
-
     /**
      * This is an admin theme.
      *
      * @var bool
      */
     protected $admin = true;
-
-    /**
-     * The module's tag.
-     *
-     * @var string
-     */
-    protected $tag = 'Anomaly\StreamsTheme\StreamsThemeTag';
 }
